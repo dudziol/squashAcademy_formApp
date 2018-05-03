88029117c147cc69f3c532f5ab844729bc72768e
dudzio <dud2iol@gmail.com>
2018-05-03 15:01:49 +0200

    app.js basic setup

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-"#" 
+var app = require("express")(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser");
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/academyForm");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+	res.render("index");
+})
+
+app.listen(3000, function(){
+	console.log("The server is running!");
+})
