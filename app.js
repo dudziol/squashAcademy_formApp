2492c70268928fbd1c89450c001d73f22069afd2
dudzio <dud2iol@gmail.com>
2018-05-03 20:44:34 +0200

    nodemailer module installed / createad files: .gitignore and .env / functionality added: sending email after submitting form, predefined env variables

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var express = require("express"),
 	app = express(),
 	mongoose = require("mongoose"),
-	bodyParser = require("body-parser");
-	expressSanitizer = require("express-sanitizer")
+	bodyParser = require("body-parser"),
+	expressSanitizer = require("express-sanitizer"),
+	dotENV = require('dotenv'),
+	nodemailer = require('nodemailer')
+	smtpTransport = require('nodemailer-smtp-transport');
 
 // APP CONFIG
+dotENV.config();
 mongoose.connect("mongodb://localhost/academyForm");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.use(expressSanitizer());
+
+// NODEMAILER CONFIG
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: process.env.MAIL_U,
+    pass: process.env.MAIL_PASSWORD
+  },
+  tls: {
+     rejectUnauthorized: false
+    }
+}));
 
 // MONGOOSE/MODEL/ CONFIG
 var contactSchema = new mongoose.Schema({
@@ -29,15 +46,27 @@ app.get("/", function(req, res){
 
 // CREATE ROUTE
 app.post("/", function(req, res){
-	// req.body.contact.body = req.sanitize(req.body.contact.body);
-	// Contact.create(req.body.contact, function(err, newContact){
-	// 	if(err){
-	// 		res.render("index");
-	// 	} else {
-	// 		res.redirect("/");
-	// 	};
-	// });
-	res.redirect("/");
+	req.body.contact.body = req.sanitize(req.body.contact.body);
+	Contact.create(req.body.contact, function(err, newContact){
+		if(err){
+			res.render("index");
+		} else {
+			let helperOptions = {
+				from: 'squashacademy.trening@gmail.com',
+				to: ,
+				subject: ,
+				text: 
+			};
+			transporter.sendMail(helperOptions, function(error, info){
+				if(error){
+					return console.log(error);
+				} else{
+					console.log("The message was sent");
+				}
+			});
+			res.redirect("/");
+		};
+	});
 });
 
 // REDIRECT to INDEX
